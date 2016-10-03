7384814c2494cc391f5d465ec81243eb7a7aa6d3
David Chang <supermacaw@gmail.com>
2016-10-03 16:29:21 -0700

    second commit

---
+++ Assets/ElectrodeScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ElectrodeScript : MonoBehaviour {
+
+	// Use this for initialization
+	public int r;
+	public int g;
+	public int b;
+
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void updateColor(float amplitude){
+		this.GetComponent<Renderer> ().material.SetColor ("_Color", new Color (1.0f-r*amplitude, 1.0f - g*amplitude, 1.0f - b*amplitude));
+	}
+}
